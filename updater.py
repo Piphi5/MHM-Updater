4d24f18fbfb11b688787e7fcc53c36a20f78208f
Piphi5 <mateus.sakata@gmail.com>
2020-09-20 10:47:44 -0500

    Initial Commit

---
+++ updater.py
@@ -0,0 +1,64 @@
+from datetime import date, timedelta
+import pandas as pd
+import requests
+import re
+import numpy as np
+import os
+
+def to_int(x):
+    try:
+        return int(x)
+    except ValueError:
+        try:
+            return int(re.sub(r".*-", "", x))
+        except ValueError:
+            return 0
+
+def geolocational_filter(gps_lat, gps_lon, recorded_lat, recorded_lon):
+    return ((recorded_lat == gps_lat and 
+        recorded_lon == gps_lon) or
+        isinstance(gps_lat, int) or
+        isinstance(gps_lon, int)
+        )
+
+
+start_date = "2017-05-29"
+end_date = date.today()
+url = f"https://api.globe.gov/search/v1/measurement/protocol/measureddate/?protocols=mosquito_habitat_mapper&startdate={start_date}&enddate={end_date}&geojson=FALSE&sample=FALSE"
+threshold = 10
+anomaly_threshold = 1000
+
+# downloads data from the GLOBE API
+response = requests.get(url)
+
+# Converts data into a useable dataframe
+data = response.json()["results"]
+        
+df = pd.DataFrame(data)
+
+# unpacking and joining the data entry
+data_df = pd.DataFrame(df["data"].to_dict())
+data_df = data_df.transpose()
+df = df.join(data_df)
+df.drop(["data"], axis=1, inplace = True)
+
+# Geolocational Filtering (Remove data with poor geolocational quality)
+vectorized_filter = np.vectorize(geolocational_filter)
+bad_data = vectorized_filter(df["mosquitohabitatmapperMeasurementLatitude"].to_numpy(),
+                             df["mosquitohabitatmapperMeasurementLongitude"].to_numpy(),
+                             df["latitude"].to_numpy(),
+                             df["longitude"].to_numpy()
+                            )
+df = df[~bad_data]
+df = df.reset_index().drop(["index"], axis = 1)
+
+# Remove suspected training events
+suspect_df = df.groupby(by=['measuredDate','latitude','mosquitohabitatmapperWaterSource','siteName','longitude']).filter(lambda x: len(x) > threshold)
+suspect_mask = df.isin(suspect_df)
+clean_df = df[~suspect_mask].dropna(how = "all")
+
+# Remove Anomolous Larvae counts
+vectorized_int = np.vectorize(to_int)
+clean_df["mosquitohabitatmapperLarvaeCount"] = vectorized_int(clean_df["mosquitohabitatmapperLarvaeCount"].fillna(0).values)
+clean_df = clean_df[clean_df["mosquitohabitatmapperLarvaeCount"] < anomaly_threshold]
+clean_df.to_csv("MHM-Data/Clean Data.csv")
